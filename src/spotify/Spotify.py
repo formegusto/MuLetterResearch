63617dfed2ad999a18e06b9316e61fc312e88a46
formegusto <formegusto@gmail.com>
2022-04-07 14:09:49 +0900

    update : Open API 사전조사 자료 comp

--- src/spotify/Spotify.py
+++ src/spotify/Spotify.py
@@ -1,5 +1,8 @@
+from src.spotify.get_reco_tracks import get_reco_tracks
 from src.spotify.get_token import get_token
 from src.spotify.search_tracks import search_tracks
+from src.spotify.get_genres import get_genres
+from src.spotify.get_features import get_features
 
 
 class Spotify:
@@ -9,3 +12,6 @@ class Spotify:
 
 Spotify.get_token = get_token
 Spotify.search_tracks = search_tracks
+Spotify.get_genres = get_genres
+Spotify.get_features = get_features
+Spotify.get_reco_tracks = get_reco_tracks
